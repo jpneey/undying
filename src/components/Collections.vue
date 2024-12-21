e8792e33adcee0b68963d9d57f0c1350c79f3325
jpneey <johnpaulburato.jp@gmail.com>
2024-12-22 01:15:32 +0800

    feat: added wip collections

--- src/components/Collections.vue
+++ src/components/Collections.vue
@@ -18,11 +18,11 @@
               <div class="border border-dark maybe-rounded p-4">
                 <div class="row align-items-center position-relative">
                   <div class="col col-12 col-md-4">
-                    <RouterLink :to="'/'+media.slug" class="box d-block position-relative overflow-hidden maybe-rounded">
+                    <div class="box d-block position-relative overflow-hidden maybe-rounded">
                       <div class="position-absolute top-50 start-50 translate-middle w-100 h-100 poster" :style="{ backgroundImage: 'url(./poster/' + media.poster + ')' }">
                       </div>
                       <div class="play position-absolute bottom-0 start-0 m-3"></div>
-                    </RouterLink>
+                    </div>
                   </div>
                   <div class="col col-12 col-md-8 mt-4 mt-md-0">
                     <div class="ps-md-4 text-white">
